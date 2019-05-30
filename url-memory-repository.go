2ab49af51c388f8003c7cf4a443946639ddff9f5
Damir15 <krosava_15d@mail.ru>
2019-05-31 02:46:13 +0600

    Changed for UrlRepository

---
+++ url-memory-repository.go
@@ -0,0 +1,44 @@
+package shortener
+
+type UrlMemoryRepository struct {
+	database map[string]string
+	err      error
+}
+
+func NewUrlMemoryRepository() *UrlMemoryRepository {
+	return &UrlMemoryRepository{
+		database: make(map[string]string),
+	}
+}
+
+func (m *UrlMemoryRepository) FindByShortUrl(url string) string {
+	if m.err != nil {
+		return ""
+	}
+
+	for longUrl, shortUrl := range m.database {
+		if shortUrl == url {
+			return longUrl
+		}
+	}
+
+	return ""
+}
+
+func (m *UrlMemoryRepository) FindByLongUrl(url string) string {
+	if m.err != nil {
+		return ""
+	}
+
+	for longUrl, shortUrl := range m.database {
+		if longUrl == url {
+			return shortUrl
+		}
+	}
+
+	return ""
+}
+
+func (m *UrlMemoryRepository) GetError() error {
+	return m.err
+}
